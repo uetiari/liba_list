e773b48885270999baca3ca4dd5ac9a460f7c2fa
uetiari <arianeueti@gmail.com>
2018-12-05 13:56:22 -0200

    Mudança para landing page

--- js/script.js
+++ js/script.js
@@ -1,132 +1,131 @@
 const data = [{
     "name" : "Kidoairaku",
-    "nacionalidade" : "Japonês",
+    "pais" : "Japão",
     "local" : "http://goo.gl/maps/Drk4da6k6az",
     "valor" : "",
     "review" : "Administrado por uma família tradicional japonesa e de artistas da cultura, o Kidoairaku proporciona comida com sabor muito próximo ao caseiro, a confort food, com porções bem generosas. Ambiente muito aconchegante. "
 },
 {
     "name" : "Rong He",
-    "nacionalidade" : "Chines",
+    "pais" : "Chinês",
     "local" : "http://goo.gl/maps/cq5Mmgv7npB2\n",
     "valor" : "",
     "review" : "Resturante de comida chinesa com muitas opções de pratos quentes e sobremesas típicas. Há possibilidade de assistir ao \"show\" que é o chefe fazendo as massas na hora através do vidro de frente ao salão. Alguns pratos tracionalmente conhecidos como Yakisoba e Gyoza são os mais pedidos, mas vale a pena optar também pelos ensopados. Pense bem se for sozinho, pois as porções são para duas ou mais pessoas."
 },
 {
     "name" : "Lamen Aska",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/vWZtPMbm3k42",
     "valor" : "",
     "review" : "Casa de lámen tradicionalíssima com caldo muito suave e gyoza muito saboroso. Existe opção de pedir Kaedama(opção quando você come todo o macarrão do seu lámen, mas ainda tem caldo e você quer comer mais macarrão) e também opção de pedir tamanhos diferenciados de lámen. Muito frequentada, pode haver fila de espera do lado de fora, o que não há problema pois a casa disponibiliza banquinho do lado de fora para aguardar ser chamado. Não aceita cartões como forma de pagamento, seja débito ou crédito, apenas dinheiro."
 },
 {
     "name" : "Izakaya Issa",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/TyH2FmwPS5m",
     "valor" : "",
     "review" : "Um tipo de restaurante não muito popular ainda na cidade. O Izakaya Issa é um tipo de boteco onde você pode apreciar uma bebida juntos de amigos, familiares com deliciosos pratos caseiros. Administrado pela dona Margarida, que optou por apenas funcionárias mulheres. Lá você pode se sentir em casa, em meio uma família apreciando uma boa comida caseira, petiscos, e comemorar alguma data importante."
 },
 {
     "name" : "Meu Udon",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/7BaYJ2F6Qem",
     "valor" : "",
     "review" : "Macarrão de Udon, é uma macarrão um pouco diferente do que estamos acostumados como por exemplo de yakissoba. Macarrão de udon é um pouco mais grosso, e tem uma técnica diferente por isso neste local ele é preparado na hora, o que garanteuma massa muito mais saborosa. Você poderá escolher vários tipos de udon no menu, de regiões diferentes do Japão, e também algumas susgestões do Chef. "
 },
 {
     "name" : "Itidai",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/aLnG8ZRioDK2",
     "valor" : "",
     "review" : "Um prato que gosto muito da culinária japonesa é o Teishoku, que é o nosso tradicional Prato Feito, muito prático com opções de carnes, conserva, tempura, além de uma porção de arroz japoonês e uma porção de misoshiru. Uma refeição muito completa, e que te faz se sentir muito satisfeito. Também existe a opção de rodízio para os adeptos. "
 },
 {
     "name" : "Petiscos do Tigrão",
-    "nacionalidade" : "Taiwan",
+    "pais" : "Taiwan",
     "local" : "http://goo.gl/maps/vPcYBaWs2q12\n",
     "valor" : "",
     "review" : "Culinária de Taiwan em específico, o resturante do Pestiscos do Tigrão ganhou muitos adeptos com um prato em específico que é o Guabao, uma espécie de sanduíche com pão cozido no vapor com recheio bem farto de carne de porco, o mais tradicional, ou então opção vegetariana com cogumelos, farofinha de amendoim e molho. "
 },
 {
     "name" : "Porque Sim",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/h8q1SvLAoFA2",
     "valor" : "",
     "review" : "Dose dupla, o Porque Sim é um restaurante no seu andar térreo e acima tem salas que podem ser reservadas para o Karaoke, você também pode fazer os pedidos enquanto espera a sua música na fila para cantar. As reservas precisam ser feitas com antecedência, porque é muito concorrido para cantar nas sala de lá. O restaurante que fica no térreo, tem muitos pratos que você pode apreciar, desde sushis, sashimis até o mai complexo como o lámen."
 },
 {
     "name" : "Kenzo Sushi",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/?cid=1162863336577325230",
     "valor" : "",
     "review" : "Sem dúvidas a melhor casa de Sushi do bairro. Casa especializada em sushi tradicional do Japão, então não vá achando que vai encontrar cream cheese ou makizushi de manga ou ainda hotrolls. Exise a opção de pedir diretamente no balcão com Sushiman ou então optar pela variedade de opções do cardápio que vai desde sashimi de cara-pau até o tão famoso e mais caro Uni. A casa prefere que você faça reservas quando for, mas nada lhe impede de ir sem reservar. Quando for, aprecie sem pressa o sabor de cada prato. Alguns amigos japoneses já disseram que é o sabor muito parecido com o Japão. "
 },
 {
     "name" : "Yuzu",
-    "nacionalidade" : "japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/WQ7Tyb1eJpq",
     "valor" : "",
     "review" : "Com uma arquitetura totalmente diferente da maioria dos locais, o Yuzu tem muito jeito de casa antiga, com direito a pé-direito de tronco de árvores, mesas de madeira pura. A comida é bem farta, teishokus, sushis e também os pedidos de petiscos. "
 },
 {
     "name" : "Ikkousha Tonkotsu Ramen",
-    "nacionalidade" : "japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/YzgjeH9jRfL2",
     "valor" : "",
     "review" : "Casa de lámen de rede japonesa, a primeira casa aqui no Brasil, é especializada em lamens com caldo de porco, chamado Tonkotsu. Ao entrar não se assuste pois todos funcionários irão dizer um sonoro \"Irashaimasê!\", o que quer dizer \"Seja bem-vindo\" em japonês. Também não há problema nenhum em fazer aquele barulho quando estiver tomando o lámen, pelo contrário, isso é do constume dos japoneses, além de ajudar você sentir melhor o gosto do seu lámen, também tem a simbologia de dizer ao chefe de que você está apreciando a comida dele. Você também pode adquirir um cartão fidelidade quando for pazer o pagamento, e novamente todos funcionários irão dizer em alto e bom tom: \"Arigatou gozaimashita!\" que também quer dizer: \"Muito obrigado!\"."
 },
 {
     "name" : "Sweet Heart",
-    "nacionalidade" : "Taiwan",
+    "pais" : "Taiwan",
     "local" : "http://goo.gl/maps/bumL5reJk2v\n",
     "valor" : "",
     "review" : "Especializado em comidas de Taiwan oferece um prato em especial que é o Hot Pot, Você customiza a sua sopa desde o caldo até os acompanhamentos, que serão feitos por você na sua mesa. O caldo vem borbulhando de quente e você vai adicionando o que pediu conforme preferir. Carnes, legumes, vegetais, você que escolhe a hora e o ponto que deseja. Também é possível encontrar nesse restaurante o chá tradicional com pobá, que são bolinhas hidratadas de tapioca."
 },
 {
     "name" : "Migá",
-    "nacionalidade" : "Coreano",
+    "pais" : "Coreano",
     "local" : "http://goo.gl/maps/E5j654o4T9v\n",
     "valor" : "",
     "review" : "Um pouquinho da comida coreana no Bairro. Banchan(pratos de acompanhamento, geralmente conservas) muito completo da culinária coreana, também é possível fazer o tradicional churrasco coreano na sua mesa. Para sobremesa vale a pena dar uma passadinha no café ao lado, que também é dos mesmos proprietários."
 },
 {
     "name" : "Top Pot",
-    "nacionalidade" : "Taiwan",
+    "pais" : "Taiwan",
     "local" : "http://goo.gl/maps/5NgoutFVv6N2",
     "valor" : "",
     "review" : "Com pouco mais de um ano de existência o Top Pot veio da ideia de duas amigas, a Chef Lilian e Chef Rosália de levar a comida de seu país para o bairro. Hot pot é um tipo de sopa, onde você recebe seu prato com um suporte com fogo para terminar o cozimento ou mantê-lo aquecido. O cardápio vem desde o mais tracional Casa Pot até os mais diferentes como o Malah Pot que tem até 3 níveis de pimenta e se você for corajosa(o) pode pedir mais dois níveis de pimenta. Sem esquecer da opção para vegetarianos e outros tipos de porções sem necessariamente serem sopa. Para acompanhar também há opção de bebida típicas como o Chá de Ervas e também o Chá de Abobrinha, com sabor muito inusitado. Para sobremesa, recomendo um doce que não está no cardápio, mas exposto no balcão do caixa, o Doce de Abacaxi em formato da ilha de Taiwan. "
 },
 {
     "name" : "Momo Lamen",
-    "nacionalidade" : "Japonês",
+    "pais": "Japão",
     "local" : "http://goo.gl/maps/VNURrdQX1UU2\n",
     "valor" : "",
     "review" : "Casa de lámen muito famosa do bairro. Segue a mesma linha das casas de lámen do bairro, com cozinha aberta onde você consegue ver todos os processos de preparação do seu lámen, você pode optar também por ficar no balcão ou em mesas, ficando ao balcão você tem esta vista. Diferente da maioria, esta passui 3 andares de para melhor conforto, mas não pense que isso irá te livrar das filas. Por ser muito próxima da estação do metrô, a maioria dos turistas preferem ir até ela, gerando filas nos horários de almoço e jantar. Possui várias opções de caldo como Shio, Shoyu, Miso, gyozas, e pratos com base de arroz como Donburi. Para beber recomendo o tradicional chá verde gelado, para os que não bebem bebida alcóolica e cervejas para os adeptos."
 }]
 
-const search = document.querySelector("input")
-const result = document.getElementById("resultado")
+const japones = document.getElementById("japones")
+const result = document.getElementById("jap-tab")
 
-
-console.log(data)
-// search.addEventListener("blur", function () {
-
-        //    .then(function(data) {
-        //     data = JSON.parse(data)
-        //     result.innerHTML = ""
-        //     if (data.items.length > 0) {
-        //         for (const rest of data.items) {
-        //             result.innerHTML += `
-        //               <h1>${rest.name}</h1>
-        //               <h2>${rest.nacionalidade}</h2>
-        //               <p>${rest.review}</p>`
-        //         }
-        //     } else {
-        //         result.innerHTML = "<p>Nenhum resultado encontrado</p>"
-        //     }
-        // })
-        // .catch(function(error) {
-        //     result.innerHTML = `<p>${error}</p>`
-        // })
-// })
-
+japones.addEventListener("click", function(e) {
+    
+        .then(function(data) {
+            data = JSON.parse(data)
+            result.innerHTML = ""
+            
+            if (data.items.length > 0) {
+                for (const rest of data.items) {
+                    data.innerHTML += `
+                        <img src="">
+                            <h1>${rest.name}</h1>
+                            <p>${rest.review}</p>
+                    `
+                }
+            } else {
+                data.innerHTML = "<p>Nenhum resultado encontrado</p>"
+            }
+        })
+        .catch(function(error) {
+            data.innerHTML = `<p>${error}</p>`
+        })
+})
